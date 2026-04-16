1ce730c69dad1850bc2d31a70a81f2d63423adac
Gust4voS10 <gustavodasilvapereira752@gmail.com>
2026-04-16 13:37:57 -0300

    quase pronto

--- ex03/ft_vault_security.py
+++ ex03/ft_vault_security.py
@@ -1,34 +1,38 @@
 
 
-def secure_archive(file_path: str, action: int, write: str) -> tuple[bool, str]:
+def secure_archive(file_path: str, action: int, write="") -> tuple[bool, str]:
     try:
         if action == 0:
             print(f"Using '{file_path}' to read from a nonexistent file")
             with open(file_path, 'r') as file:
-                data_fragment = file.read()
-                print(f"True, {data_fragment}\n")
-                return [True, data_fragment]
+                data = file.read()
+            return [True, data]
         elif action == 1:
             print(f"Using '{file_path}' to "
                   f"write previous content to a new file")
             with open(file_path, 'w') as file:
                 file.write(write)
-                print("True, Content successfully written to filezn\n")
-                return [True, "'Content successfully written to file'"]
+            return [True, 'Content successfully written to file']
+        else:
+            return [False, "Invalid action specified."]
 
     except FileNotFoundError:
-        print(f"False, [Errno 2] No such file or directory: '{file_path}'\n")
+        return [False, f"[Errno 2] No such file or directory: '{file_path}'"]
 
     except PermissionError:
         print(f"(False, [Errno 13] Permission denied: '{file_path}'")
 
 
 def main() -> None:
-    print("=== Cyber Archives Security ===")
-    secure_archive("c.txt", 0, "a")
-    secure_archive("bca.txt", 0, "a")
-    secure_archive("abc.txt", 0, "a")
-    secure_archive("a", 1, "abcbcabac")
+    print("=== Cyber Archives Security ===\n")
+    print(secure_archive("c.txt", 0))
+    print()
+    print(secure_archive("t2.txt", 0))
+    print()
+    print(secure_archive("t1.txt", 0))
+    print()
+    print(secure_archive("a", 1, "abcbcabac"))
+    print()
 
 
 if __name__ == "__main__":
