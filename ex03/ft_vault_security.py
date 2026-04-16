cbd682a831b96aad56a0d15bc8606b278d9b9556
Gust4voS10 <gustavodasilvapereira752@gmail.com>
2026-04-16 13:09:04 -0300

    falta alguns ajustes

--- ex03/ft_vault_security.py
+++ ex03/ft_vault_security.py
@@ -6,22 +6,29 @@ def secure_archive(file_path: str, action: int, write: str) -> tuple[bool, str]:
             print(f"Using '{file_path}' to read from a nonexistent file")
             with open(file_path, 'r') as file:
                 data_fragment = file.read()
+                print(f"True, {data_fragment}\n")
+                return [True, data_fragment]
         elif action == 1:
-            print(f"Using '{file_path}' to write previous content to a new file")
+            print(f"Using '{file_path}' to "
+                  f"write previous content to a new file")
             with open(file_path, 'w') as file:
                 file.write(write)
-        return [True, data_fragment]
+                print("True, Content successfully written to filezn\n")
+                return [True, "'Content successfully written to file'"]
 
-    except (FileExistsError, PermissionError) as e:
-        return [False, e]
+    except FileNotFoundError:
+        print(f"False, [Errno 2] No such file or directory: '{file_path}'\n")
+
+    except PermissionError:
+        print(f"(False, [Errno 13] Permission denied: '{file_path}'")
 
 
 def main() -> None:
     print("=== Cyber Archives Security ===")
-    print(secure_archive("c.txt", 0, "a"))
-    print(secure_archive("bca.txt", 0, "a"))
-    print(secure_archive("abc.txt", 0, "a"))
-    print(secure_archive("a", 1, "abcbcabac"))
+    secure_archive("c.txt", 0, "a")
+    secure_archive("bca.txt", 0, "a")
+    secure_archive("abc.txt", 0, "a")
+    secure_archive("a", 1, "abcbcabac")
 
 
 if __name__ == "__main__":
